a21fd0eb16f67471488cc611e13e6618f8a4612c
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-09-23 21:20:53 +0900

    ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,33 @@
 import { RestaurantsModule } from './restarants/restaurants.module';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
 import { ApolloDriver } from '@nestjs/apollo';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+console.log(Joi.string());
 @Module({
   imports: [
-    RestaurantsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'minjakim',
-      password: '000000',
-      database: 'uber-eats',
-      synchronize: true,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PROT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       logging: true,
     }),
     GraphQLModule.forRoot({
@@ -23,6 +35,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       // autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       autoSchemaFile: true,
     }),
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
