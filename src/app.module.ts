53bdd6818a615a7159caaa0105bb3fc00c75b563
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-01-03 17:23:15 +0900

    Configuring

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,25 +4,31 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { PodcastModule } from './podcast/podcast.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
     RestaurantsModule,
     PodcastModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'minjakim',
-      password: '12345',
-      database: 'uber-eats',
-      synchronize: true,
-      logging: true,
-    }),
   ],
   controllers: [],
   providers: [],
