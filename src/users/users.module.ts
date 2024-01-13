ac957792171ca4226f8d545b419420f25431d9aa
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-01-13 15:05:40 +0900

    create Account

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersResolver } from './users.resolver';
+import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UsersResolver, UsersService],
+})
 export class UsersModule {}
