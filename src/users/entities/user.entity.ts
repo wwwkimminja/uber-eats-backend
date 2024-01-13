ffdbe4f62f545d0e0f587189b06ea166601de261
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-01-13 15:05:20 +0900

    user model

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,16 +1,45 @@
+import {
+  Field,
+  InputType,
+  ObjectType,
+  registerEnumType,
+} from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
-import { Column, Entity } from 'typeorm';
+import { BeforeInsert, Column, Entity } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { InternalServerErrorException } from '@nestjs/common';
 
-type UserRole = 'client' | 'owner' | 'delivery';
+enum UserRole {
+  Client,
+  Owner,
+  Delivery,
+}
 
+registerEnumType(UserRole, { name: 'UserRole' });
+
+@InputType({ isAbstract: true })
+@ObjectType()
 @Entity()
 export class User extends CoreEntity {
   @Column()
+  @Field((type) => String)
   email: string;
 
   @Column()
+  @Field((type) => String)
   password: string;
 
-  @Column()
+  @Column({ type: 'enum', enum: UserRole })
+  @Field((type) => UserRole)
   role: UserRole;
+
+  @BeforeInsert()
+  async hasPassword(): Promise<void> {
+    try {
+      this.password = await bcrypt.hash(this.password, 10);
+    } catch (e) {
+      console.log(e);
+      throw new InternalServerErrorException();
+    }
+  }
 }
