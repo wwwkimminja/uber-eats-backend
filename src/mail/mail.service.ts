4d00800625e6c17452ecb6ce8788c111ce8c51c7
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-03-20 20:47:35 +0900

    feat:send Email

---
+++ src/mail/mail.service.ts
@@ -0,0 +1,35 @@
+import got from 'got';
+import * as FormData from 'form-data';
+import { Inject, Injectable } from '@nestjs/common';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { MailModuleOptions } from './main.interfaces';
+
+@Injectable()
+export class MailService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
+  ) {
+    this.sendEmail('texting', 'test', 'mandojjang@gmail.com');
+  }
+
+  private async sendEmail(subject: string, content: string, to: string) {
+    const form = new FormData();
+    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+    form.append('to', to);
+    form.append('subject', subject);
+    form.append('text', content);
+    const response = await got(
+      `https://api.mailgun.net/v3/${this.options.domain}/messages `,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `api:${this.options.apiKey}`,
+          ).toString('base64')}`,
+        },
+        body: form,
+      },
+    );
+    console.log(response.body);
+  }
+}
