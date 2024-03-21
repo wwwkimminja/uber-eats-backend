7459d3f659ce09fb6ae58cc359b81a6be146b7f4
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-03-21 19:56:43 +0900

    sendVerificationEmail

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,42 +2,48 @@ import got from 'got';
 import * as FormData from 'form-data';
 import { Inject, Injectable } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
-import { MailModuleOptions } from './main.interfaces';
+import { EmailVar, MailModuleOptions } from './main.interfaces';
 
 @Injectable()
 export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
-  ) {
-    this.sendEmail('texting', 'test', 'mandojjang@gmail.com', 'verify-email');
-  }
+  ) {}
 
   private async sendEmail(
     subject: string,
-    content: string,
-    to: string,
     template: string,
+    emailVars: EmailVar[],
   ) {
     const form = new FormData();
-    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
-    form.append('to', to);
+    form.append('from', `Nico from Uber Eats <mailgun@${this.options.domain}>`);
+    form.append('to', 'mandojjang@gmail.com');
     form.append('subject', subject);
     // form.append('text', content);
     form.append('template', template);
-    form.append('v:code', 'test-code');
-    form.append('v:username', 'username');
-    const response = await got(
-      `https://api.mailgun.net/v3/${this.options.domain}/messages `,
-      {
-        method: 'POST',
-        headers: {
-          Authorization: `Basic ${Buffer.from(
-            `api:${this.options.apiKey}`,
-          ).toString('base64')}`,
+    emailVars.forEach((eVar) => form.append(`v:${eVar.key}`, eVar.value));
+    try {
+      const response = await got(
+        `https://api.mailgun.net/v3/${this.options.domain}/messages `,
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Basic ${Buffer.from(
+              `api:${this.options.apiKey}`,
+            ).toString('base64')}`,
+          },
+          body: form,
         },
-        body: form,
-      },
-    );
-    console.log(response.body);
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  sendVerificationEmail(email: string, code: string) {
+    this.sendEmail('Verify Your Email', 'verify-email', [
+      { key: 'code', value: code },
+      { key: 'username', value: email },
+    ]);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,3 +1,4 @@
+import { MailService } from './../mail/mail.service';
 import { JwtService } from './../jwt/jwt.service';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -21,6 +22,7 @@ export class UsersService {
     @InjectRepository(Verification)
     private readonly verifications: Repository<Verification>,
     private readonly jwtService: JwtService,
+    private readonly mailService: MailService,
   ) {}
 
   async createAccount({
@@ -36,11 +38,12 @@ export class UsersService {
       const user = await this.users.save(
         this.users.create({ email, password, role }),
       );
-      await this.verifications.save(
+      const verification = await this.verifications.save(
         this.verifications.create({
           user,
         }),
       );
+      this.mailService.sendVerificationEmail(user.email, verification.code);
       return { ok: true };
     } catch (e) {
       return { ok: false, error: "Couldn't create account" };
@@ -99,7 +102,10 @@ export class UsersService {
       if (email) {
         user.email = email;
         user.verified = false;
-        await this.verifications.save(this.verifications.create({ user }));
+        const verification = await this.verifications.save(
+          this.verifications.create({ user }),
+        );
+        this.mailService.sendVerificationEmail(user.email, verification.code);
       }
       if (password) {
         user.password = password;
@@ -137,7 +143,6 @@ export class UsersService {
         ok: false,
         error: 'Verification not found',
       };
-      throw new Error();
     } catch (error) {
       return {
         ok: false,
