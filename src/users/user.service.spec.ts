2a6a04d02b2a14e02b6d975854ae7e8e9af7fefa
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-03-24 15:36:37 +0900

    mocking userRepository

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -5,6 +5,7 @@ import { User } from './entities/user.entity';
 import { Verification } from './entities/verification.entity';
 import { JwtService } from 'src/jwt/jwt.service';
 import { MailService } from 'src/mail/mail.service';
+import { Repository } from 'typeorm';
 
 const mockRepository = {
   findOne: jest.fn(),
@@ -18,8 +19,12 @@ const mockJwtService = {
 const mockMailService = {
   sendVerificationEmail: jest.fn(),
 };
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
 describe('UserService', () => {
   let service: UsersService;
+  let userRepository: MockRepository<User>;
   beforeAll(async () => {
     const module = await Test.createTestingModule({
       providers: [
@@ -43,11 +48,14 @@ describe('UserService', () => {
       ],
     }).compile();
     service = module.get<UsersService>(UsersService);
+    userRepository = module.get(getRepositoryToken(User));
   });
   it('be defined', () => {
     expect(service).toBeDefined();
   }),
-    it.todo('createAccount');
+    describe('createAccount', () => {
+      it('should fail if user exists', () => {});
+    });
   it.todo('login');
   it.todo('findById');
   it.todo('editProfile');
